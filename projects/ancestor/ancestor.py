a95a04ff214555ce7da66b007ec1fa1ee8e20239
ash12hub <ash12hub@users.noreply.github.com>
2020-08-03 16:36:04 +0530

    Earliest Ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,32 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    children = {}
+    for ancestor in ancestors:
+        if ancestor[1] not in children:
+            children[ancestor[1]] = set()
+            children[ancestor[1]].add(ancestor[0])
+        else:
+            children[ancestor[1]].add(ancestor[0])
+
+    visited = set()
+    stack = []
+    stack.append([starting_node])
+    max_length = 1
+    earliest = -1
+    while len(stack) > 0:
+        current_path = stack.pop()
+        current_node = current_path[-1]
+
+        if len(current_path) > max_length or len(current_path) == max_length and current_node < earliest:
+            max_length = len(current_path)
+            earliest = current_node
+
+        if current_node not in visited:
+            visited.add(current_node)
+            if current_node in children:
+                parents = children[current_node]
+                for parent in parents:
+                    parent_copy = current_path.copy()
+                    parent_copy.append(parent)
+                    stack.append(parent_copy)
+    return earliest
