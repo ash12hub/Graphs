4bc9e18767d2eb37ac7d35b92217a991bc893e60
ash12hub <ash12hub@users.noreply.github.com>
2020-08-03 16:36:36 +0530

    Adventure Sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,6 +28,61 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# traversal_graph = {'0': {'n': ?, 's': ?}}
+traversal_graph = {}
+
+# Get exits in starting room
+traversal_graph[0] = {}
+exits = player.current_room.get_exits()
+for exit in exits:
+    traversal_graph[0][exit] = '?'
+
+new_room = False
+while True:
+    if new_room:
+        new_room = False
+        exits = player.current_room.get_exits()
+        for exit in exits:
+            if exit not in traversal_graph[player.current_room.id]:
+                traversal_graph[player.current_room.id][exit] = '?'
+
+    next_room_found = False            
+    for exit in traversal_graph[player.current_room.id]:
+        if traversal_graph[player.current_room.id][exit] == '?':
+            next_room_found = True
+            previous_room = player.current_room
+            entrance = ''
+            if exit == 'n':
+                entrance = 's'
+            elif exit == 's':
+                entrance = 'n'
+            elif exit == 'w':
+                entrance = 'e'
+            elif exit == 'e':
+                entrance = 'w'
+            traversal_path.append(exit)
+            player.travel(exit)
+            traversal_graph[previous_room.id][exit] = player.current_room.id
+            if player.current_room.id not in traversal_graph:
+                traversal_graph[player.current_room.id] = {}
+                traversal_graph[player.current_room.id][entrance] = previous_room.id
+                traversal_graph[player.current_room.id]['first_visit'] = entrance
+                new_room = True
+            # print(previous_room.id, traversal_graph[previous_room.id])
+            # print(player.current_room.id, traversal_graph[player.current_room.id])
+            break
+    if len(traversal_graph) == len(room_graph):
+        break
+    
+    if not next_room_found:
+        prerious_room = player.current_room
+        # print(player.current_room.id)
+        exit = traversal_graph[player.current_room.id]['first_visit']
+        traversal_path.append(exit)
+        player.travel(exit)
+        
+
+
 
 
 
@@ -50,13 +105,13 @@ else:
 
 #######
 # UNCOMMENT TO WALK AROUND
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
